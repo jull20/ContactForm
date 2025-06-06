53ab42ac53e8aa363bafceb407b780a21aa578c8
Yulia Zulina <>
2025-06-06 17:13:25 +0300

    popka

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from 'react'
+import './css/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
+  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // if(checkFormEmpty()){
+    // }
+    let formData = new FormData(event.currentTarget);
+    let data = Object.fromEntries(formData);
+    console.log(data)
+  }
+  const checkFormEmpty = (formData: FormData) => {
+    let data = Object.fromEntries(formData);
+    console.log(data)
+    // return false;
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="main">
+      <div className="mainContainer">
+        <form className='form' action={checkFormEmpty}>
+          <fieldset className='mainFieldset'>
+            <legend className='contactUsLegend'>Contact Us</legend>
+            <Input type="text" name="firstName" id="firstName">First Name <span className='star'>*</span></Input>
+            <Input type="text" name="lastName" id="lastName">Last Name <span className='star'>*</span></Input>
+            <Input type="email" name="email" id="email">Email Address <span className='star'>*</span></Input>
+            <fieldset className='queryTypeFieldset'>
+              <legend className='queryTypeLegend'>Query Type <span className='star'>*</span></legend>
+              <Input type='radio' name="queryType" id="generalEnquiry" inputValue="generalEnquiry">General Enquiry</Input>
+              <Input type='radio' name="queryType" id="supportRequest" inputValue="supportRequest">Support Request</Input>
+            </fieldset>
+            <div className="inputEl messageContainer">
+              <label className='label' htmlFor="message">Message: <span className='star'>*</span></label>
+              <textarea id="message" name="message" rows={3}></textarea>
+            </div>
+          </fieldset>
+          <Input type='checkbox' name="checkbox" id="checkbox">I consent to being contacted by the team <span className='star'>*</span></Input>
+          
+          <button type="submit" className='submitBtn'>Submit</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
-export default App
+type TextInputProps = {
+  children: React.ReactNode;
+  name: string; 
+  id: string;
+  type:string;
+  inputValue?:string;
+}
+
+function Input({name, id, type, children, inputValue=""}:TextInputProps){
+  let inputAttributesValue = (inputValue !== "") ? {value: inputValue} : {};
+  return (
+    <div className='inputEl'>
+      <label className='label' htmlFor={name}>{children}</label>
+      <input type={type} id={id} name={name} {...inputAttributesValue}/>
+    </div>
+  )
+}
